e7e63f6041b44eb5376fc19bc7fcf40e292e5a64
edgar-badalyan <edgar.badalyan1999@gmail.com>
2023-02-06 19:36:04 +0100

    Add cloud utils + cleanup

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     Image.MAX_IMAGE_PIXELS = None
 
     # Change to fit your dataset path
-    DATASET_PATH = "../../../data/Clouds_2D"
+    DATASET_PATH = "../../../data/Clouds_intensity"
 
     # You can use this if your dataset has the given format
     # Otherwise, check json_loader.py to see how
@@ -27,7 +27,7 @@ if __name__ == "__main__":
 
     # If not using default config defined in utils.py,
     # you can pass arguments using this dictionary
-    cfg_args = {"OUTPUT_DIR": "output_Cloud2D_10000"}
+    cfg_args = {"OUTPUT_DIR": "output_Clouds_intensity"}
 
     # Uncomment this line to train
     # train_loop(loader, augs, cfg_args=cfg_args)
